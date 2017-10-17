118167671b7acdc7d2b2e53ce95ced65f0d66cc9
khouryj <khouryj6@gmail.com>
2017-10-17 12:34:32 -0400

    buzzfizz

---
+++ ArrayFizzBuzz/ArrayFizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arr = new String[100];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + ". ";
+                if (i % 3 == 0)
+                {
+                    arr[i] += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    arr[i] += "Buzz";
+                }
+            }
+            foreach (String s in arr)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
