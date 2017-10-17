ca71352aea8142df5959cd0a07f3d064ef64cc41
khouryj <khouryj6@gmail.com>
2017-10-17 14:02:06 -0400

    UniqueDigits

---
+++ UniqueDigits/UniqueDigits/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[50];
+            Random rnd = new Random();
+            bool unique;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(1, 50);
+            }
+
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                unique = true;
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        unique = false;
+                    }
+                }
+                for (int j = nums.Length-1; j > nums[i]; j--)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        unique = false;
+                    }
+                }
+                if (unique)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
+        }
+    }
+}
